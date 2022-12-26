2f06afdc30fca3c3d5ecd9b663669ba119509d66
Iurii Topychkanov <44700542+iurii-topychkanov@users.noreply.github.com>
2022-12-26 18:14:01 +0300

    added handling of error detail with array/object types (#131)

--- web/src/shared/helpers/get-error.tsx
+++ web/src/shared/helpers/get-error.tsx
@@ -1,14 +1,53 @@
 import { ApiCallError } from '@epam/uui';
 
-export const getError = (error: any) => {
-    const apiCallError = error as ApiCallError;
-    let errorMessage = 'Error occurred';
+enum DETAIL {
+    STRING = 'string',
+    OBJECT = 'object',
+    ARRAY = 'array'
+}
 
-    if (apiCallError.call?.responseData) {
-        const responseData = error.call.responseData as any;
-        errorMessage = responseData.errorMessage || responseData?.detail;
-    } else if (error.details.detail) {
-        errorMessage = error.details.detail;
+const DEFAULT_ERROR_MESSAGE = 'Error occurred';
+
+export const getError = (error: any) => {
+    try {
+        const apiCallError = error as ApiCallError;
+        let errorMessage = DEFAULT_ERROR_MESSAGE;
+
+        if (apiCallError.call?.responseData) {
+            const responseData = error.call.responseData as any;
+            errorMessage = responseData.errorMessage || responseData?.detail;
+        } else if (error.details.detail) {
+            const detailType = extractDetailType(error.details.detail);
+
+            switch (detailType) {
+                case DETAIL.STRING:
+                    return error.details.detail;
+                case DETAIL.OBJECT:
+                    return error.details.detail.msg;
+                case DETAIL.ARRAY:
+                    return error.details.detail[0].msg;
+                default:
+                    return proccedUnhandledType(detailType);
+            }
+        }
+
+        return errorMessage;
+    } catch (e) {
+        console.error('Something went wrong', e);
+
+        return DEFAULT_ERROR_MESSAGE;
     }
-    return errorMessage;
+};
+
+const extractDetailType = (detail: any): DETAIL => {
+    const detailType = typeof detail as DETAIL;
+
+    if (detailType === 'object') {
+        return Array.isArray(detail) ? DETAIL.ARRAY : detailType;
+    }
+    return detailType;
+};
+
+const proccedUnhandledType = (x: never): never => {
+    throw new Error(`Unhandled type was used: ${x}`);
 };
