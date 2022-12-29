8553c5af315139bb15420d88059f0cf21daa507a
thinklab <anton_alekseev2@epam.com>
2022-12-29 17:49:13 +0300

    fix review notes, revert user_id changes

--- annotation/app/microservice_communication/search.py
+++ annotation/app/microservice_communication/search.py
@@ -195,8 +195,8 @@ def expand_response(
             "name": job_names.get(task.job_id, None),
         }
         response_task["user"] = {
-            "id": str(task.user_id),
-            "name": user_logins.get(task.user_id),
+            "id": response_task.get("user_id"),
+            "name": user_logins.get(response_task.get("user_id")),
         }
         response.append(ExpandedManualAnnotationTaskSchema(**response_task))
     return response
