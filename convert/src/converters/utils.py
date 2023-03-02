85522c003fa65249f64bf991bdf43c7b706eb1de
Sergei Smolov <sergei_smolov@epam.com>
2023-03-02 20:20:57 +0400

    refactor: refactor filtering method

--- convert/src/converters/utils.py
+++ convert/src/converters/utils.py
@@ -5,19 +5,27 @@ from src.converters.base_format.models.tokens import BadgerdocToken
 
 def filter_printing_tokens(objs: List[BadgerdocToken]) -> List[BadgerdocToken]:
     needed_objs = []
-    is_start_string = True
-    non_printable_sequence = []
+    at_the_beginning_of_string = True
+    non_printing_sequence = []
     for token_obj in objs:
         char = token_obj.text
-        if char.isspace() and is_start_string:
-            non_printable_sequence.append(char)
-            continue
         if not char.isspace():
-            is_start_string = False
             needed_objs.append(token_obj.copy())
-            if non_printable_sequence:
-                needed_objs[0].previous = "".join(non_printable_sequence)
-            continue
+
+        # form field previous
+        if at_the_beginning_of_string:
+            if char.isspace():
+                non_printing_sequence.append(char)
+                continue
+            else:
+                needed_objs[0].previous = (
+                    "".join(non_printing_sequence)
+                    if non_printing_sequence
+                    else None
+                )
+                at_the_beginning_of_string = False
+
+        # form field after
         if char.isspace():
             previous_text = needed_objs[-1].after
             needed_objs[-1].after = (
