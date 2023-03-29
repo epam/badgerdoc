f711aeff1d179ad98df57652a9c50bc84832b07a
Anna Iustus <anna_iustus@epam.com>
2023-03-29 16:46:00 +0200

    fix: add safe navigator operator for dataAttributes reading

--- web/src/components/task/task-sidebar-flow/annotation.tsx
+++ web/src/components/task/task-sidebar-flow/annotation.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react';
-import { Text } from '@epam/loveship';
-import styles from './styles.module.scss';
 import { Annotation } from 'shared';
 import { stringToRGBA } from 'shared/components/annotator/utils/string-to-rgba';
 import { ANNOTATION_FLOW_ITEM_ID_PREFIX } from 'shared/constants/annotations';
 import { ANNOTATION_PATH_SEPARATOR } from './constants';
+import { Text } from '@epam/loveship';
+import styles from './styles.module.scss';
 
 export const AnnotationRow: FC<
     Annotation & {
