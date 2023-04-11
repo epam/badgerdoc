0cc0d000e47f24054c42e989d3808336493033e9
Fedor Spiridonov <76848937+spiridonovfed@users.noreply.github.com>
2023-04-11 12:26:11 +0400

    fix tasks/search endpoint in annotation - case when user does not exist (#518)

--- annotation/annotation/microservice_communication/user.py
+++ annotation/annotation/microservice_communication/user.py
@@ -5,6 +5,7 @@ import requests
 from dotenv import find_dotenv, load_dotenv
 from requests import RequestException
 
+from annotation.logger import Logger
 from annotation.models import ManualAnnotationTask
 
 load_dotenv(find_dotenv())
@@ -34,6 +35,9 @@ def get_response(callback_url: str, user_id: str, tenant: str, token: str):
             raise GetUserInfoAccessDenied(
                 "Access Denied on request to users microservice "
             )
+        if user_response.status_code == 404:
+            Logger.info("User %s is not found in 'users'", user_id)
+            return ""
         if user_response.status_code != 200:
             raise GetUserInfoException(
                 f"Failed request to 'users' microservice: {user_response.text}"
