9f12b0ac74df0d26ecea3e5388e9f32a95ea961e
Igor Odinochenkov <40606828+nanbratan@users.noreply.github.com>
2023-04-13 17:00:31 +0600

    feat: Added possibility to handle links at flow sidebar (#519)
    Closes #497
    
    Co-authored-by: Igor Odinochenkov <Igor_Odinochenkov1@epam.com>

--- web/src/components/task/task-sidebar-flow/annotation.tsx
+++ web/src/components/task/task-sidebar-flow/annotation.tsx
@@ -5,15 +5,38 @@ import { ANNOTATION_FLOW_ITEM_ID_PREFIX } from 'shared/constants/annotations';
 import { ANNOTATION_PATH_SEPARATOR } from './constants';
 import { Text } from '@epam/loveship';
 import { cx } from '@epam/uui';
+
 import styles from './styles.module.scss';
+import { Link } from 'api/typings';
+import { Links } from './links';
 
 export const AnnotationRow: FC<
     Annotation & {
         index: number;
+        incomingLinks?: Annotation['links'];
+        annotationNameById: Record<string, string>;
         onSelect: (index: number) => void;
+        onSelectById: (id: Annotation['id']) => void;
         selectedAnnotationId?: Annotation['id'];
+        isEditable: boolean;
+        onLinkDeleted: (pageNum: number, annotationId: Annotation['id'], link: Link) => void;
     }
-> = ({ id, color = '', label = '', text, index, onSelect, categoryName, selectedAnnotationId }) => {
+> = ({
+    id,
+    color = '',
+    label = '',
+    text,
+    index,
+    onSelect,
+    onSelectById,
+    categoryName,
+    selectedAnnotationId,
+    annotationNameById,
+    incomingLinks,
+    links,
+    isEditable,
+    onLinkDeleted
+}) => {
     const labelList = label.split('.');
 
     return (
@@ -24,14 +47,26 @@ export const AnnotationRow: FC<
             className={cx(styles.item, id === selectedAnnotationId && styles.selectedAnnotation)}
         >
             <div
+                className={styles.label}
                 style={{
                     color,
                     backgroundColor: stringToRGBA(color, 0.2)
                 }}
             >
-                <Text cx={styles.label} rawProps={{ 'data-testid': 'flow-label' }}>
+                <Text cx={styles.labelText} rawProps={{ 'data-testid': 'flow-label' }}>
                     {labelList[labelList.length - 1]}
                 </Text>
+                {!links?.length && !incomingLinks?.length ? null : (
+                    <Links
+                        isEditable={isEditable}
+                        onSelect={onSelectById}
+                        links={links}
+                        annotationId={id}
+                        incomingLinks={incomingLinks}
+                        onLinkDeleted={onLinkDeleted}
+                        annotationNameById={annotationNameById}
+                    />
+                )}
             </div>
             {categoryName !== label && (
                 <Text
---
+++ web/src/components/task/task-sidebar-flow/links/link-row.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from 'react';
+import { FlexRow, IconContainer, Text } from '@epam/loveship';
+import { Annotation } from 'shared';
+
+import { ReactComponent as crossIcon } from '@epam/assets/icons/common/navigation-close-18.svg';
+
+import styles from './styles.module.scss';
+
+export const LinkRow: FC<{
+    to: Annotation['id'];
+    onDelete: () => void;
+    annotationName: string;
+    onClose?: () => void;
+    isEditable: boolean;
+    onSelect: (id: Annotation['id']) => void;
+    icon: React.FunctionComponent<
+        React.SVGProps<SVGSVGElement> & {
+            title?: string | undefined;
+        }
+    >;
+}> = ({ onClose, onSelect, onDelete, to, annotationName, icon, isEditable }) => (
+    <FlexRow
+        cx={styles['link-row']}
+        onClick={(event) => {
+            event.stopPropagation();
+            onSelect(to);
+            onClose?.();
+        }}
+    >
+        <div className={styles.title}>
+            <IconContainer icon={icon} color="cobalt" />
+            <Text>{annotationName}</Text>
+        </div>
+        {isEditable && (
+            <IconContainer
+                cx={styles['cross']}
+                icon={crossIcon}
+                onClick={(event) => {
+                    event.stopPropagation();
+                    onDelete();
+                }}
+            />
+        )}
+    </FlexRow>
+);
