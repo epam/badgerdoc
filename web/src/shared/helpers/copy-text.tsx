b7319b0e0f3a734e28a51225794bcf1538c3b575
Svetlana Bilevich <svetlana.bilevich@gmail.com>
2023-04-28 12:24:47 +0400

    fix: copy doc name to clopboard

--- web/src/shared/helpers/copy-text.tsx
+++ web/src/shared/helpers/copy-text.tsx
@@ -3,16 +3,37 @@ import { SuccessNotification, Text } from '@epam/loveship';
 import { INotification } from '@epam/uui';
 import { svc } from 'services';
 
+const unsecuredCopyToClipboard = (filename: string) => {
+    const textArea = document.createElement('textarea');
+    textArea.value = filename;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+        document.execCommand('copy');
+        showCopied();
+    } catch (err) {
+        console.error('Unable to copy to clipboard', err);
+    }
+    document.body.removeChild(textArea);
+};
+
+const showCopied = () => {
+    svc.uuiNotifications.show(
+        (props: INotification) => (
+            <SuccessNotification {...props}>
+                <Text>Copied to clipboard! </Text>
+            </SuccessNotification>
+        ),
+        { duration: 2 }
+    );
+};
+
 export const handleCopy = (e: React.MouseEvent<SVGSVGElement>, filename: string) => {
     e.stopPropagation();
-    navigator.clipboard.writeText(filename).then(() => {
-        svc.uuiNotifications.show(
-            (props: INotification) => (
-                <SuccessNotification {...props}>
-                    <Text>Copied to clipboard! </Text>
-                </SuccessNotification>
-            ),
-            { duration: 2 }
-        );
-    });
+    if (window.isSecureContext && navigator.clipboard) {
+        navigator.clipboard.writeText(filename).then(showCopied);
+    } else {
+        unsecuredCopyToClipboard(filename);
+    }
 };
