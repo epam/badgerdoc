e1f554b3fa5a13b457003f30037c94559e42813c
mikemalkhasyanvertex <Mike.Malkhasyan@vertexinc.com>
2023-06-02 14:59:48 +0400

    - remove unnecessary query param from fetch latest annotations request

--- web/src/api/hooks/annotations.ts
+++ web/src/api/hooks/annotations.ts
@@ -97,11 +97,10 @@ async function fetchLatestAnnotations(
     pageNumbers?: number[],
     userId?: string
 ): Promise<AnnotationsResponse> {
-    const pageNums = pageNumbers?.map((pageNumber) => `page_numbers=${pageNumber}`);
     const revId = revisionId || 'latest';
     const user = userId ? `&user_id=${userId}` : '';
     return useBadgerFetch<AnnotationsResponse>({
-        url: `${namespace}/annotation/${jobId}/${fileId}/${revId}?${pageNums?.join('&')}${user}`,
+        url: `${namespace}/annotation/${jobId}/${fileId}/${revId}?${user}`,
         method: 'get',
         withCredentials: true
     })();
