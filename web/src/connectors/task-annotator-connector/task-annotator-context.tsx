2c588173af0e096a6bf269df93a9d6566b0eec0d
abdulAziz Nurov <nurovaziz@gmail.com>
2023-06-06 15:30:43 +0500

    fix: Make a separate state for Annotation Type only (#674)

--- web/src/connectors/task-annotator-connector/task-annotator-context.tsx
+++ web/src/connectors/task-annotator-connector/task-annotator-context.tsx
@@ -97,6 +97,7 @@ type ContextValue = SplitValidationValue &
         isOwner: boolean;
         taskUsers: MutableRefObject<TTaskUsers>;
         selectionType: AnnotationBoundType | AnnotationLinksBoundType | AnnotationImageToolType;
+        annotationType: AnnotationBoundType;
         selectedTool: AnnotationImageToolType;
         setSelectedTool: (t: AnnotationImageToolType) => void;
         onChangeSelectedTool: (t: AnnotationImageToolType) => void;
@@ -212,6 +213,7 @@ export const TaskAnnotatorContextProvider: React.FC<ProviderProps> = ({
     const [selectionType, setSelectionType] = useState<
         AnnotationBoundType | AnnotationLinksBoundType | AnnotationImageToolType
     >('free-box');
+    const [annotationType, setAnnotationType] = useState<AnnotationBoundType>('box');
     const [selectedTool, setSelectedTool] = useState<AnnotationImageToolType>('pen');
     const [selectedAnnotation, setSelectedAnnotation] = useState<Annotation | undefined>();
     const [isDataTabDisabled, setIsDataTabDisabled] = useState<boolean>(dataTabDefaultDisableState);
@@ -307,6 +309,12 @@ export const TaskAnnotatorContextProvider: React.FC<ProviderProps> = ({
             refetchCategories();
         }
     }, [task, jobId]);
+
+    useEffect(() => {
+        if (['box', 'free-box', 'table', 'text', 'table_cell', 'polygon'].includes(selectionType)) {
+            setAnnotationType(selectionType as AnnotationBoundType);
+        }
+    }, [selectionType]);
 
     const documentFilters: FilterWithDocumentExtraOption<keyof FileDocument>[] = [];
 
@@ -1066,6 +1074,7 @@ export const TaskAnnotatorContextProvider: React.FC<ProviderProps> = ({
             setPageSize,
             modifiedPages,
             selectionType,
+            annotationType,
             selectedTool,
             setSelectedTool,
             selectedToolParams,
