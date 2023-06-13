00e798a3a299398a6863530bd2c7b398f1229991
Anton Alekseev2 <anton_alekseev2@epam.com>
2023-06-13 14:40:11 +0300

    add timing

--- convert/convert/models/pdf.py
+++ convert/convert/models/pdf.py
@@ -6,3 +6,7 @@ from convert.models.common import S3Path
 class PdfRequest(BaseModel):
     input_pdf: S3Path
     output_tokens: S3Path
+
+
+class PDFResponse(BaseModel):
+    runtime: str
--- convert/convert/routers/pdf.py
+++ convert/convert/routers/pdf.py
@@ -1,10 +1,12 @@
+import datetime
+
 from fastapi import APIRouter, status
 
 from convert.config import minio_client
 from convert.converters.pdf.pdf_to_badgerdoc_converter import (
     PDFToBadgerdocConverter,
 )
-from convert.models.pdf import PdfRequest
+from convert.models.pdf import PdfRequest, PDFResponse
 
 router = APIRouter(prefix="/pdf", tags=["pdf"])
 
@@ -13,11 +15,15 @@ router = APIRouter(prefix="/pdf", tags=["pdf"])
     "/import",
     status_code=status.HTTP_201_CREATED,
 )
-def import_pdf(request: PdfRequest) -> None:
+def import_pdf(request: PdfRequest) -> PDFResponse:
+    before = datetime.datetime.now()
     pdf_to_bd_use_case = PDFToBadgerdocConverter(
         s3_client=minio_client,
     )
     pdf_to_bd_use_case.execute(
         s3_input_pdf=request.input_pdf,
         s3_output_tokens=request.output_tokens,
+    )
+    return PDFResponse(
+        runtime=f"runtime: {str(datetime.datetime.now() - before)}"
     )
