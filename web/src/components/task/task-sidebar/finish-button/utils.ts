6c6714519b8a4bf8ca13a015e8d07db73f79984d
Arthur Bortolini <82807813+ammb95@users.noreply.github.com>
2023-09-20 16:45:16 +0200

    fix: Tooltip With Very Long List Of Pages (#739)
    Co-authored-by: Arthur Bortolini <arthur_bortolini@epam.com>

--- web/src/components/task/task-sidebar/finish-button/utils.ts
+++ web/src/components/task/task-sidebar/finish-button/utils.ts
@@ -1,7 +1,27 @@
+const LISTED_PAGES_LIMIT = 10;
+
+const getPagesList = (pages: number[]) => pages.join(', ');
+
+const getAboveLimitPagesCount = (pages: number[]) => pages.length - LISTED_PAGES_LIMIT;
+
+const getVisiblePagesArray = (pages: number[]) => pages.slice(0, LISTED_PAGES_LIMIT);
+
+const getVisiblePagesString = (pages: number[]) => getPagesList(getVisiblePagesArray(pages));
+
+const listRemainingPages = (pages: number[]) => {
+    if (pages.length > LISTED_PAGES_LIMIT) {
+        const visiblePages = getVisiblePagesString(pages);
+        const remainingPagesAboveLimit = getAboveLimitPagesCount(pages);
+
+        return `${visiblePages} and ${remainingPagesAboveLimit} more.`;
+    }
+    return getPagesList(pages);
+};
+
 export const createTooltip = (isExtCov: boolean, notProcessedPages: number[]): string => {
     return isExtCov
         ? `Please wait for all annotators to finish their tasks`
-        : `Please validate all page to finish task. Remaining pages: ${notProcessedPages?.join(
-              ', '
+        : `Please validate all page to finish task. Remaining pages: ${listRemainingPages(
+              notProcessedPages
           )}`;
 };
