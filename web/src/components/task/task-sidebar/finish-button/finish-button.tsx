114c1d018e9229cecdd25c09c954c5091d95ab57
Andrei Dubovik <andrei_dubovik@epam.com>
2023-09-21 11:53:16 +0300

    IECSSP-545

--- web/src/components/task/task-sidebar/finish-button/finish-button.tsx
+++ web/src/components/task/task-sidebar/finish-button/finish-button.tsx
@@ -1,12 +1,22 @@
 import React, { FC, useState } from 'react';
 import { useArrayDataSource } from '@epam/uui';
-import { PickerInput, Button, ControlGroup, Tooltip } from '@epam/loveship';
+import {
+    PickerInput,
+    Button,
+    ControlGroup,
+    Tooltip,
+    WarningNotification,
+    FlexRow,
+    Text
+} from '@epam/loveship';
 
 import { ReactComponent as settingsIcon } from '@epam/assets/icons/common/navigation-more_vert-18.svg';
 import styles from './finish-button.module.scss';
 import { ValidationType } from '../../../../api/typings';
 import { TaskStatus } from '../../../../api/typings/tasks';
 import { createTooltip } from './utils';
+import { ConfirmModal } from './confirm-modal/confirm-modal';
+import { useUuiContext } from '@epam/uui-core';
 
 type TaskSidebarProps = {
     viewMode: boolean;
@@ -45,12 +55,15 @@ export const FinishButton: FC<TaskSidebarProps> = ({
         localStorage.getItem('submitted-task-redirection-page') ?? 'tasks'
     );
 
+    const confirmWindowNeed = process.env.REACT_APP_FINISH_LABELING_CONFIRM;
+
     const handleSetRedirectionSettings = (value: 'tasks' | 'next-task') => {
         setRedirectionSettings(value);
         localStorage.setItem('submitted-task-redirection-page', value);
     };
 
     const handleFinishValidation = isSplitValidation ? onFinishSplitValidation : onFinishValidation;
+    const { uuiModals, uuiNotifications } = useUuiContext();
 
     let isDisabled = !isAnnotatable;
     if (jobType !== 'extensive_coverage') {
@@ -76,45 +89,70 @@ export const FinishButton: FC<TaskSidebarProps> = ({
     );
 
     return !isValidation && viewMode ? null : (
-        <ControlGroup cx={styles['button-finish-control-group']}>
-            <Tooltip content={tooltipContent}>
-                <Button
-                    isDisabled={isDisabled}
-                    cx={styles['button-finish']}
-                    caption={isValidation ? 'FINISH VALIDATION' : 'FINISH LABELING'}
-                    onClick={
-                        isValidation
-                            ? () => {
-                                  handleFinishValidation();
-                              }
-                            : onAnnotationTaskFinish
-                    }
-                />
-            </Tooltip>
-            {isNextTaskPresented && (
-                <PickerInput
-                    valueType="id"
-                    minBodyWidth={322}
-                    selectionMode="single"
-                    value={redirectionSettings}
-                    dataSource={redirectSettingsDataSource}
-                    getName={(item) => item?.caption ?? ''}
-                    onValueChange={handleSetRedirectionSettings}
-                    renderToggler={({ toggleDropdownOpening, ...props }) => (
-                        <Button
-                            {...props}
-                            size="36"
-                            fill="solid"
-                            isDropdown={false}
-                            icon={settingsIcon}
-                            onClear={undefined}
-                            placeholder={undefined}
-                            isDisabled={isDisabled}
-                            onClick={toggleDropdownOpening}
-                        />
-                    )}
-                />
-            )}
-        </ControlGroup>
+        <>
+            <ControlGroup cx={styles['button-finish-control-group']}>
+                <Tooltip content={tooltipContent}>
+                    <Button
+                        isDisabled={isDisabled}
+                        cx={styles['button-finish']}
+                        caption={isValidation ? 'FINISH VALIDATION' : 'FINISH LABELING'}
+                        onClick={() => {
+                            if (confirmWindowNeed === 'true') {
+                                uuiModals
+                                    .show<string>((props) => <ConfirmModal {...props} />)
+                                    .then(() => {
+                                        isValidation
+                                            ? () => {
+                                                  handleFinishValidation();
+                                              }
+                                            : onAnnotationTaskFinish;
+                                    })
+                                    .catch(() => {
+                                        uuiNotifications
+                                            .show((props) => (
+                                                <WarningNotification {...props}>
+                                                    <FlexRow alignItems="center">
+                                                        <Text>Finishing aborted.</Text>
+                                                    </FlexRow>
+                                                </WarningNotification>
+                                            ))
+                                            .catch(() => null);
+                                    });
+                            } else {
+                                isValidation
+                                    ? () => {
+                                          handleFinishValidation();
+                                      }
+                                    : onAnnotationTaskFinish;
+                            }
+                        }}
+                    />
+                </Tooltip>
+                {isNextTaskPresented && (
+                    <PickerInput
+                        valueType="id"
+                        minBodyWidth={322}
+                        selectionMode="single"
+                        value={redirectionSettings}
+                        dataSource={redirectSettingsDataSource}
+                        getName={(item) => item?.caption ?? ''}
+                        onValueChange={handleSetRedirectionSettings}
+                        renderToggler={({ toggleDropdownOpening, ...props }) => (
+                            <Button
+                                {...props}
+                                size="36"
+                                fill="solid"
+                                isDropdown={false}
+                                icon={settingsIcon}
+                                onClear={undefined}
+                                placeholder={undefined}
+                                isDisabled={isDisabled}
+                                onClick={toggleDropdownOpening}
+                            />
+                        )}
+                    />
+                )}
+            </ControlGroup>
+        </>
     );
 };
