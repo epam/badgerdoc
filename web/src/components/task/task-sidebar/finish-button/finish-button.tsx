f3b24cfdc94fee30e7f8a6a4d9564c6d736b03e0
Andrei Dubovik <andrei_dubovik@epam.com>
2023-09-27 12:36:06 +0300

    fix: removed useCallback from import in finishButton

--- web/src/components/task/task-sidebar/finish-button/finish-button.tsx
+++ web/src/components/task/task-sidebar/finish-button/finish-button.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useState } from 'react';
+import React, { FC, useState } from 'react';
 import { useArrayDataSource } from '@epam/uui';
 import { PickerInput, Button, ControlGroup, Tooltip } from '@epam/loveship';
 
