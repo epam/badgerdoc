451373f8adbc999a11b6c94b4e4f6dec1fafc13f
Yury Ostroukh <0str0uchura@gmail.com>
2023-10-02 13:39:13 +0300

    refactor: move TaskPage's logic under TaskAnnotatorContext

--- web/src/pages/task/task-page.tsx
+++ web/src/pages/task/task-page.tsx
@@ -24,14 +24,25 @@ import { ReactComponent as goNextIcon } from '@epam/assets/icons/common/navigati
 import { ReactComponent as goPrevIcon } from '@epam/assets/icons/common/navigation-chevron-up-18.svg';
 import { Labels } from 'shared/components/labels';
 import { FlexSpacer } from '@epam/uui-components';
+import { History } from 'history';
 
-const TaskPage: FC = () => {
+type TTaskPageContent = {
+    taskId: string;
+    handleRedirectToNextTask: () => void;
+    history: History<Record<string, string | undefined>>;
+    nextTaskId: number | undefined;
+};
+
+const TaskPageContent: FC<TTaskPageContent> = ({
+    taskId,
+    handleRedirectToNextTask,
+    history,
+    nextTaskId
+}) => {
     const [additionalScale, setAdditionalScale] = useState(0);
     const [gridVariant, setGridVariant] = useState(GridVariants.horizontal);
     const [goToPage, setGoToPage] = useState(1);
 
-    const { pathname } = useLocation();
-    const { taskId } = useParams<{ taskId: string }>();
     const taskData = useTaskById({ taskId: Number(taskId) }, { enabled: Boolean(taskId) })?.data;
     const isLastPage = goToPage === taskData?.pages.length;
     const isFirstPage = goToPage === 1;
@@ -66,6 +77,113 @@ const TaskPage: FC = () => {
         isFirstPage ? setGoToPage(1) : setGoToPage((prev) => prev - 1);
     }, [goToPage, taskData?.pages, isFirstPage]);
 
+    const crumbs = useMemo(() => {
+        const crumbs = [];
+        const { previousPage, previousPageName, previousPageUrl } = history.location.state ?? {};
+
+        if (previousPage === PREVIOUS_PAGE_JOB) {
+            crumbs.push({ name: 'Extractions', url: JOBS_PAGE });
+            crumbs.push({
+                name: previousPageName ?? '',
+                url: previousPageUrl
+            });
+        } else {
+            crumbs.push({ name: 'My Tasks', url: DASHBOARD_PAGE });
+        }
+
+        crumbs.push({ name: 'Task' });
+
+        return crumbs;
+    }, [history.location.state]);
+
+    return (
+        <div className="flex-col">
+            <div className={styles.title}>
+                <div className={styles['title__left-block']}>
+                    <FlexRow spacing="12" cx={styles['full-width']}>
+                        <FlexCell width="auto" minWidth={320}>
+                            <BreadcrumbNavigation breadcrumbs={crumbs} />
+                        </FlexCell>
+                        <FlexCell width="100%">
+                            <FlexRow>
+                                <Labels />
+                                <FlexSpacer />
+                                <FlexRow cx={styles['goto-page-selector']}>
+                                    <FlexCell minWidth={60}>
+                                        <span>Go to page</span>
+                                    </FlexCell>
+                                    <PickerInput
+                                        minBodyWidth={52}
+                                        size="24"
+                                        dataSource={pagesDataSource}
+                                        value={goToPage}
+                                        onValueChange={onPageChange}
+                                        getName={(item) => String(item)}
+                                        selectionMode="single"
+                                        disableClear={true}
+                                    />
+                                    <FlexRow>
+                                        <span>of {taskData?.pages.length}</span>
+                                        <Button
+                                            size="24"
+                                            fill="white"
+                                            icon={goPrevIcon}
+                                            cx={styles.button}
+                                            onClick={handleGoPrev}
+                                            isDisabled={isFirstPage}
+                                        />
+                                        <Button
+                                            size="24"
+                                            fill="white"
+                                            icon={goNextIcon}
+                                            cx={styles.button}
+                                            onClick={handleGoNext}
+                                            isDisabled={isLastPage}
+                                        />
+                                    </FlexRow>
+                                </FlexRow>
+                                <DocumentScale
+                                    scale={additionalScale}
+                                    onChange={setAdditionalScale}
+                                />
+                            </FlexRow>
+                        </FlexCell>
+                    </FlexRow>
+                </div>
+                <div className={styles['title__right-block']}>
+                    <div>
+                        <PickGridType value={gridVariant} onChange={setGridVariant} />
+                    </div>
+                    {nextTaskId && (
+                        <Button
+                            size="30"
+                            fill="white"
+                            caption="Next"
+                            cx={styles['next-button']}
+                            onClick={handleRedirectToNextTask}
+                        />
+                    )}
+                </div>
+            </div>
+            <div className={styles.content}>
+                <TableAnnotatorContextProvider>
+                    <FlowSideBar />
+                    <TaskDocumentPages
+                        viewMode={false}
+                        gridVariant={gridVariant}
+                        goToPage={goToPage}
+                        additionalScale={additionalScale}
+                    />
+                    <TaskSidebar viewMode={false} isNextTaskPresented={Boolean(nextTaskId)} />
+                </TableAnnotatorContextProvider>
+            </div>
+        </div>
+    );
+};
+
+const TaskPage: FC = () => {
+    const { pathname } = useLocation();
+    const { taskId } = useParams<{ taskId: string }>();
     const history = useHistory<Record<string, string | undefined>>();
     const { notifySuccess, notifyError } = useNotifications();
     const nextTaskId = useNextAndPreviousTask(taskId).data?.next_task?.id;
@@ -104,25 +222,6 @@ const TaskPage: FC = () => {
         );
     };
 
-    const crumbs = useMemo(() => {
-        const crumbs = [];
-        const { previousPage, previousPageName, previousPageUrl } = history.location.state ?? {};
-
-        if (previousPage === PREVIOUS_PAGE_JOB) {
-            crumbs.push({ name: 'Extractions', url: JOBS_PAGE });
-            crumbs.push({
-                name: previousPageName ?? '',
-                url: previousPageUrl
-            });
-        } else {
-            crumbs.push({ name: 'My Tasks', url: DASHBOARD_PAGE });
-        }
-
-        crumbs.push({ name: 'Task' });
-
-        return crumbs;
-    }, [history.location.state]);
-
     return (
         <TaskAnnotatorContextProvider
             taskId={Number(taskId)}
@@ -130,87 +229,12 @@ const TaskPage: FC = () => {
             onSaveTaskSuccess={handleSaveTaskSuccess}
             onSaveTaskError={handleSaveTaskError}
         >
-            <div className="flex-col">
-                <div className={styles.title}>
-                    <div className={styles['title__left-block']}>
-                        <FlexRow spacing="12" cx={styles['full-width']}>
-                            <FlexCell width="auto" minWidth={320}>
-                                <BreadcrumbNavigation breadcrumbs={crumbs} />
-                            </FlexCell>
-                            <FlexCell width="100%">
-                                <FlexRow>
-                                    <Labels />
-                                    <FlexSpacer />
-                                    <FlexRow cx={styles['goto-page-selector']}>
-                                        <FlexCell minWidth={60}>
-                                            <span>Go to page</span>
-                                        </FlexCell>
-                                        <PickerInput
-                                            minBodyWidth={52}
-                                            size="24"
-                                            dataSource={pagesDataSource}
-                                            value={goToPage}
-                                            onValueChange={onPageChange}
-                                            getName={(item) => String(item)}
-                                            selectionMode="single"
-                                            disableClear={true}
-                                        />
-                                        <FlexRow>
-                                            <span>of {taskData?.pages.length}</span>
-                                            <Button
-                                                size="24"
-                                                fill="white"
-                                                icon={goPrevIcon}
-                                                cx={styles.button}
-                                                onClick={handleGoPrev}
-                                                isDisabled={isFirstPage}
-                                            />
-                                            <Button
-                                                size="24"
-                                                fill="white"
-                                                icon={goNextIcon}
-                                                cx={styles.button}
-                                                onClick={handleGoNext}
-                                                isDisabled={isLastPage}
-                                            />
-                                        </FlexRow>
-                                    </FlexRow>
-                                    <DocumentScale
-                                        scale={additionalScale}
-                                        onChange={setAdditionalScale}
-                                    />
-                                </FlexRow>
-                            </FlexCell>
-                        </FlexRow>
-                    </div>
-                    <div className={styles['title__right-block']}>
-                        <div>
-                            <PickGridType value={gridVariant} onChange={setGridVariant} />
-                        </div>
-                        {nextTaskId && (
-                            <Button
-                                size="30"
-                                fill="white"
-                                caption="Next"
-                                cx={styles['next-button']}
-                                onClick={handleRedirectToNextTask}
-                            />
-                        )}
-                    </div>
-                </div>
-                <div className={styles.content}>
-                    <TableAnnotatorContextProvider>
-                        <FlowSideBar />
-                        <TaskDocumentPages
-                            viewMode={false}
-                            gridVariant={gridVariant}
-                            goToPage={goToPage}
-                            additionalScale={additionalScale}
-                        />
-                        <TaskSidebar viewMode={false} isNextTaskPresented={Boolean(nextTaskId)} />
-                    </TableAnnotatorContextProvider>
-                </div>
-            </div>
+            <TaskPageContent
+                taskId={taskId}
+                nextTaskId={nextTaskId}
+                history={history}
+                handleRedirectToNextTask={handleRedirectToNextTask}
+            ></TaskPageContent>
         </TaskAnnotatorContextProvider>
     );
 };
