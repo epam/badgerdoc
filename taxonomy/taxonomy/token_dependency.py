3a32deb987a85ea907359e262130a0b320c09d73
Denis Rybakov <minefrs@gmail.com>
2024-05-02 20:22:47 +0300

    fix

--- taxonomy/taxonomy/token_dependency.py
+++ taxonomy/taxonomy/token_dependency.py
@@ -6,7 +6,7 @@ import os
 
 from tenant_dependency import TenantData, get_tenant_info
 
-KEYCLOAK_HOST = os.getenv('KEYCLOAK_HOST')
+KEYCLOAK_HOST = os.getenv("KEYCLOAK_HOST")
 TOKEN = get_tenant_info(url=KEYCLOAK_HOST, algorithm="RS256")
 
 if os.getenv("TAXONOMY_NO_AUTH", False):
