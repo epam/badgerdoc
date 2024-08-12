e936dc2ac5d87223a7e91a7dca6f4601a26ce476
Filip Negojevic <filip_negojevic@epam.com>
2024-08-12 14:24:14 +0200

    resolved conversations 2

--- annotation/tests/test_jobs.py
+++ annotation/tests/test_jobs.py
@@ -1,108 +1,90 @@
-import pytest
-from unittest.mock import MagicMock, patch 
-
 import uuid
-from annotation.jobs.services import update_inner_job_status, check_annotators
+from typing import Set, Tuple
+from unittest.mock import MagicMock
+from uuid import UUID
+
+import pytest
+
+from annotation.errors import FieldConstraintError
+from annotation.jobs.services import check_annotators, update_inner_job_status
 from annotation.models import Category, File, Job, User
 from annotation.schemas import (
     CategoryTypeSchema,
     FileStatusEnumSchema,
-    TaskStatusEnumSchema,
-    ValidationSchema,
     JobStatusEnumSchema,
+    ValidationSchema,
 )
 
-from annotation.errors import (
-    EnumValidationError,
-    FieldConstraintError,
-    WrongJobError,
-)
 
-JOB_TENANT = "test"
-ANNOTATORS = (
-    User(user_id="82533770-a99e-4873-8b23-6bbda86b59ae"),
-    User(user_id="ef81a4d0-cc01-447b-9025-a70ed441672d"),
-)
-CATEGORIES = [
-    Category(
+@pytest.fixture
+def job_annotators():
+    yield (
+        User(user_id="82533770-a99e-4873-8b23-6bbda86b59ae"),
+        User(user_id="ef81a4d0-cc01-447b-9025-a70ed441672d"),
+    )
+
+
+@pytest.fixture
+def categories():
+    yield Category(
         id="18d3d189e73a4680bfa77ba3fe6ebee5",
         name="Test",
         type=CategoryTypeSchema.box,
     )
-]
 
-JOB_IDS = [1, 2, 3]
-JOBS_TO_TEST_PROGRESS = (
-    Job(  # Annotation job with 3 tasks in progress, 1 finished task
-        job_id=JOB_IDS[0],
+
+@pytest.fixture
+def jobs_to_test_progress(job_annotators: Tuple[User], categories: Category):
+    yield Job(
+        job_id=1,
         callback_url="http://www.test.com",
-        annotators=[
-            ANNOTATORS[0],
-            ANNOTATORS[1],
-        ],
+        annotators=[job_annotators[0], job_annotators[1]],
         validation_type=ValidationSchema.cross,
         is_auto_distribution=False,
-        categories=CATEGORIES,
+        categories=categories,
         deadline="2021-10-19T01:01:01",
-        tenant=JOB_TENANT,
+        tenant="test",
+    )
+
+
+@pytest.fixture
+def files(jobs_to_test_progress: Job):
+    yield File(
+        file_id=1,
+        tenant="test",
+        job_id=jobs_to_test_progress[0].job_id,
+        pages_number=5,
+        status=FileStatusEnumSchema.pending,
+    )
+
+
+def test_update_inner_job_status():
+    mock_session = MagicMock()
+    update_inner_job_status(mock_session, 1, JobStatusEnumSchema.finished)
+    mock_session.query(Job).filter(Job.job_id == 1).update.assert_called_with(
+        {"status": JobStatusEnumSchema.finished}
+    )
+
+
+@pytest.mark.parametrize(
+    (
+        "validation_type",
+        "annotators",
     ),
-    Job(  # Extraction job with no tasks
-        job_id=JOB_IDS[1],
-        callback_url="http://www.test.com",
-        annotators=[],
-        validation_type=ValidationSchema.cross,
-        is_auto_distribution=False,
-        categories=CATEGORIES,
-        deadline="2021-10-19T01:01:01",
-        tenant=JOB_TENANT,
-    ),
-    Job(  # Job with all tasks in progress
-        job_id=JOB_IDS[2],
-        callback_url="http://www.test.com",
-        annotators=[
-            ANNOTATORS[0],
-            ANNOTATORS[1],
-        ],
-        validation_type=ValidationSchema.cross,
-        is_auto_distribution=False,
-        categories=CATEGORIES,
-        deadline="2021-10-19T01:01:01",
-        tenant=JOB_TENANT,
+    (
+        (
+            ValidationSchema.cross,
+            {uuid.UUID("82533770-a99e-4873-8b23-6bbda86b59ae")},
+        ),
+        (ValidationSchema.hierarchical, set()),
+        (
+            ValidationSchema.validation_only,
+            {uuid.UUID("82533770-a99e-4873-8b23-6bbda86b59ae")},
+        ),
     ),
 )
-# FILE_TEST_PROGRESS = File(
-#     file_id=1,
-#     tenant=JOB_TENANT,
-#     job_id=JOB_IDS[0],
-#     pages_number=5,
-#     status=FileStatusEnumSchema.pending,
-# )
-# FILE_TEST_PROGRESS_2 = File(
-#     file_id=2,
-#     tenant=JOB_TENANT,
-#     job_id=JOB_IDS[1],
-#     pages_number=5,
-#     status=FileStatusEnumSchema.pending,
-# )
-# FILE_TEST_PROGRESS_3 = File(
-#     file_id=3,
-#     tenant=JOB_TENANT,
-#     job_id=JOB_IDS[2],
-#     pages_number=5,
-#     status=FileStatusEnumSchema.pending,
-# )
-
-def test_update_job_status():
-
-    mock_session = MagicMock()
-
-    update_inner_job_status(mock_session, JOB_IDS[0], JobStatusEnumSchema.finished)
-
-    mock_session.query().filter().update.assert_called_with({"status":JobStatusEnumSchema.finished})
-
-def test_check_annotators():
-    validation_type = ValidationSchema.cross
-    annotators = set([uuid.UUID(ANNOTATORS[0].user_id)]) # we must first create a list to use set() function
-
+def test_check_annotators(
+    validation_type: ValidationSchema, annotators: Set[UUID]
+):
     with pytest.raises(FieldConstraintError):
         check_annotators(annotators, validation_type)
