e1f86737595417977e08f519fba991e6c7f5800e
Filip Negojevic <filip_negojevic@epam.com>
2024-08-12 14:23:28 +0200

    resolved conversations 2

--- annotation/tests/test_jobs.py
+++ annotation/tests/test_jobs.py
@@ -15,9 +15,6 @@ from annotation.schemas import (
     ValidationSchema,
 )
 
-JOB_TENANT = "test"
-JOB_IDS = (1, 2, 3)
-
 
 @pytest.fixture
 def job_annotators():
@@ -37,44 +34,36 @@ def categories():
 
 
 @pytest.fixture
-def jobs_to_test_progress(
-    job_annotators: Tuple[User], categories: Tuple[Category]
-):
-    yield (
-        Job(
-            job_id=JOB_IDS[0],
-            callback_url="http://www.test.com",
-            annotators=[job_annotators[0], job_annotators[1]],
-            validation_type=ValidationSchema.cross,
-            is_auto_distribution=False,
-            categories=categories,
-            deadline="2021-10-19T01:01:01",
-            tenant=JOB_TENANT,
-        ),
+def jobs_to_test_progress(job_annotators: Tuple[User], categories: Category):
+    yield Job(
+        job_id=1,
+        callback_url="http://www.test.com",
+        annotators=[job_annotators[0], job_annotators[1]],
+        validation_type=ValidationSchema.cross,
+        is_auto_distribution=False,
+        categories=categories,
+        deadline="2021-10-19T01:01:01",
+        tenant="test",
     )
 
 
 @pytest.fixture
-def files(jobs_to_test_progress: Tuple[Job]):
-    yield (
-        File(
-            file_id=1,
-            tenant=JOB_TENANT,
-            job_id=jobs_to_test_progress[0].job_id,
-            pages_number=5,
-            status=FileStatusEnumSchema.pending,
-        ),
+def files(jobs_to_test_progress: Job):
+    yield File(
+        file_id=1,
+        tenant="test",
+        job_id=jobs_to_test_progress[0].job_id,
+        pages_number=5,
+        status=FileStatusEnumSchema.pending,
     )
 
 
 def test_update_inner_job_status():
     mock_session = MagicMock()
-    update_inner_job_status(
-        mock_session, JOB_IDS[0], JobStatusEnumSchema.finished
+    update_inner_job_status(mock_session, 1, JobStatusEnumSchema.finished)
+    mock_session.query(Job).filter(Job.job_id == 1).update.assert_called_with(
+        {"status": JobStatusEnumSchema.finished}
     )
-    mock_session.query(Job).filter(
-        Job.job_id == JOB_IDS[0]
-    ).update.assert_called_with({"status": JobStatusEnumSchema.finished})
 
 
 @pytest.mark.parametrize(
@@ -85,24 +74,12 @@ def test_update_inner_job_status():
     (
         (
             ValidationSchema.cross,
-            {
-                uuid.UUID(
-                    User(
-                        user_id="82533770-a99e-4873-8b23-6bbda86b59ae"
-                    ).user_id
-                )
-            },
+            {uuid.UUID("82533770-a99e-4873-8b23-6bbda86b59ae")},
         ),
         (ValidationSchema.hierarchical, set()),
         (
             ValidationSchema.validation_only,
-            {
-                uuid.UUID(
-                    User(
-                        user_id="82533770-a99e-4873-8b23-6bbda86b59ae"
-                    ).user_id
-                )
-            },
+            {uuid.UUID("82533770-a99e-4873-8b23-6bbda86b59ae")},
         ),
     ),
 )
