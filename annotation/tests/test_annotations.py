9061a7dd2f6e196d0729f159ef43a2ed565e2cf9
djo753 <145800147+djo753@users.noreply.github.com>
2024-08-23 14:49:35 +0200

    test(annotation): cover construct document links (#914)

--- annotation/tests/test_annotations.py
+++ annotation/tests/test_annotations.py
@@ -25,6 +25,7 @@ from annotation.annotations.main import (
     check_null_fields,
     connect_s3,
     construct_annotated_doc,
+    construct_document_links,
     construct_particular_rev_response,
     convert_bucket_name_if_s3prefix,
     create_manifest_json,
@@ -43,12 +44,20 @@ from annotation.annotations.main import (
     upload_json_to_minio,
     upload_pages_to_minio,
 )
-from annotation.models import AnnotatedDoc, Category, File, Job, User
+from annotation.models import (
+    AnnotatedDoc,
+    Category,
+    DocumentLinks,
+    File,
+    Job,
+    User,
+)
 from annotation.schemas.annotations import (
     AnnotatedDocSchema,
     DocForSaveSchema,
     PageSchema,
     ParticularRevisionSchema,
+    RevisionLink,
 )
 from annotation.schemas.categories import CategoryTypeSchema
 from annotation.schemas.jobs import JobTypeEnumSchema, ValidationSchema
@@ -1207,3 +1216,25 @@ def test_accumulate_pages_info_unique_status(annotated_doc: AnnotatedDoc):
     assert not_proc == {3}
     assert categ == annotated_doc.categories
     assert required_rev is None
+
+
+def test_construct_document_links(annotated_doc: AnnotatedDoc):
+    rev_link = RevisionLink(
+        revision=annotated_doc.revision,
+        job_id=annotated_doc.job_id,
+        file_id=annotated_doc.file_id,
+        label="similar",
+    )
+    expected_links = [
+        DocumentLinks(
+            original_revision=annotated_doc.revision,
+            original_file_id=annotated_doc.file_id,
+            original_job_id=annotated_doc.job_id,
+            similar_revision=rev_link.revision,
+            similar_file_id=rev_link.file_id,
+            similar_job_id=rev_link.job_id,
+            label=rev_link.label,
+        )
+    ]
+    links = construct_document_links(annotated_doc, [rev_link])
+    assert links == expected_links
