61046ab662921773a5ba35ff6740a525005719e8
Negoje <37124594+Negoje@users.noreply.github.com>
2024-08-23 09:49:36 -0700

    test(annotation): cover jobs.services.py functions (#917)
    Co-authored-by: djo753 <145800147+djo753@users.noreply.github.com>
    Co-authored-by: Filip Negojevic <filip_negojevic@epam.com>

--- annotation/tests/test_jobs.py
+++ annotation/tests/test_jobs.py
@@ -7,34 +7,27 @@ from uuid import UUID
 import pytest
 
 from annotation.errors import FieldConstraintError
-from annotation.jobs.services import (
-    JobNotFoundError,
-    check_annotators,
-    collect_job_names,
-    get_job_attributes_for_post,
-    get_jobs_by_files,
-    get_jobs_by_name,
-    get_pages_in_work,
-    get_tasks_to_delete,
-    update_inner_job_status,
-    update_jobs_categories,
-    update_jobs_names,
-)
+from annotation.jobs import services
 from annotation.models import Category, File, Job, ManualAnnotationTask, User
 from annotation.schemas import (
     CategoryTypeSchema,
     FileStatusEnumSchema,
+    JobInfoSchema,
     JobStatusEnumSchema,
+    JobTypeEnumSchema,
     TaskStatusEnumSchema,
     ValidationSchema,
 )
 
 
 @pytest.fixture
-def job_annotators():
+def users():
     yield (
-        User(user_id="82533770-a99e-4873-8b23-6bbda86b59ae"),
-        User(user_id="ef81a4d0-cc01-447b-9025-a70ed441672d"),
+        User(user_id=UUID(int=0)),
+        User(user_id=UUID(int=1)),
+        User(user_id=UUID(int=2)),
+        User(user_id=UUID(int=3)),
+        User(user_id=UUID(int=4)),
     )
 
 
@@ -48,113 +41,87 @@ def categories():
 
 
 @pytest.fixture
-def jobs_to_test_progress(job_annotators: Tuple[User], categories: Category):
+def jobs_to_test_progress(users: Tuple[User], categories: Category):
     yield (
         Job(
             job_id=1,
             name="test1",
             callback_url="http://www.test.com",
-            annotators=[job_annotators[0], job_annotators[1]],
+            annotators=list(users[:2]),
             validation_type=ValidationSchema.cross,
             is_auto_distribution=False,
             categories=[categories],
             deadline="2021-10-19T01:01:01",
             tenant="test",
+            extensive_coverage=1,
         ),
         Job(
             job_id=2,
             name="test2",
             callback_url="http://www.test.com",
-            annotators=[job_annotators[0], job_annotators[1]],
-            validation_type=ValidationSchema.cross,
+            annotators=list(users[:2]),
+            validation_type=ValidationSchema.validation_only,
             is_auto_distribution=False,
             categories=[categories],
             deadline="2021-10-19T01:01:01",
             tenant="test",
+            extensive_coverage=1,
         ),
     )
 
 
 @pytest.fixture
 def tasks():
-    yield (
-        ManualAnnotationTask(
-            id=1,
-            file_id=1,
-            pages=[1],
-            job_id=1,
-            user_id="7b626e68-857d-430a-b65b-bba0a40417ee",
-            is_validation=False,
-            status=TaskStatusEnumSchema.in_progress,
-            deadline=None,
-        ),
-        ManualAnnotationTask(
-            id=2,
-            file_id=2,
-            pages=[1],
-            job_id=2,
-            user_id="7b626e68-857d-430a-b65b-bba0a40417ee",
-            is_validation=False,
-            status=TaskStatusEnumSchema.finished,
-            deadline=None,
-        ),
-        ManualAnnotationTask(
-            id=3,
-            file_id=3,
-            pages=[1],
-            job_id=2,
-            user_id="7b626e68-857d-430a-b65b-bba0a40417ea",
-            is_validation=False,
-            status=TaskStatusEnumSchema.ready,
-            deadline=None,
-        ),
-        ManualAnnotationTask(
-            id=4,
-            file_id=4,
-            pages=[1],
-            job_id=10,
-            user_id="7b626e68-857d-430a-b65b-bba0a40417eb",
-            is_validation=False,
-            status=TaskStatusEnumSchema.pending,
-            deadline=None,
-        ),
-        ManualAnnotationTask(
-            id=5,
-            file_id=5,
-            pages=[3],
-            job_id=10,
-            user_id="7b626e68-857d-430a-b65b-bba0a40417eb",
-            is_validation=True,
-            status=TaskStatusEnumSchema.ready,
-            deadline=None,
-        ),
-        ManualAnnotationTask(
-            id=6,
-            file_id=1,
-            pages=[1],
-            job_id=10,
-            user_id="7b626e68-857d-430a-b65b-bba0a40417eb",
-            is_validation=True,
-            status=TaskStatusEnumSchema.pending,
-            deadline=None,
-        ),
+    status = (
+        TaskStatusEnumSchema.finished,
+        TaskStatusEnumSchema.in_progress,
+        TaskStatusEnumSchema.ready,
+        TaskStatusEnumSchema.pending,
+        TaskStatusEnumSchema.ready,
+        TaskStatusEnumSchema.pending,
     )
+    tasks = []
+    for i in range(1, 7):
+        tasks.append(
+            ManualAnnotationTask(
+                id=i,
+                file_id=i if i != 6 else 1,
+                pages=[1] if i != 5 else [3],
+                job_id=i if i < 4 else 10,
+                user_id="7b626e68-857d-430a-b65b-bba0a40417ee",
+                is_validation=False if i < 5 else True,
+                status=status[i - 1],
+                deadline=None,
+            )
+        )
+    yield tasks
 
 
 @pytest.fixture
-def files(jobs_to_test_progress: Job):
-    yield File(
-        file_id=1,
-        tenant="test",
-        job_id=jobs_to_test_progress[0].job_id,
-        pages_number=5,
-        status=FileStatusEnumSchema.pending,
+def files():
+    yield (
+        File(
+            file_id=1,
+            tenant="tenant",
+            job_id=1,
+            pages_number=100,
+            status=FileStatusEnumSchema.pending,
+        ),
+        File(
+            file_id=2,
+            tenant="tenant",
+            job_id=1,
+            pages_number=150,
+            status=FileStatusEnumSchema.pending,
+        ),
     )
 
 
 def test_update_inner_job_status():
     mock_session = MagicMock()
-    update_inner_job_status(mock_session, 1, JobStatusEnumSchema.finished)
+    services.update_inner_job_status(
+        mock_session, 1, JobStatusEnumSchema.finished
+    )
     mock_session.query(Job).filter(Job.job_id == 1).update.assert_called_with(
         {"status": JobStatusEnumSchema.finished}
     )
@@ -181,7 +148,7 @@ def test_check_annotators(
     validation_type: ValidationSchema, annotators: Set[UUID]
 ):
     with pytest.raises(FieldConstraintError):
-        check_annotators(annotators, validation_type)
+        services.check_annotators(annotators, validation_type)
 
 
 def test_collect_job_names_all_db(jobs_to_test_progress: Tuple[Job, ...]):
@@ -191,7 +158,7 @@ def test_collect_job_names_all_db(jobs_to_test_progress: Tuple[Job, ...]):
         jobs_to_test_progress[1],
     ]
     expected_result = {1: "test1", 2: "test2"}
-    result = collect_job_names(mock_session, [1, 2], "test", "token")
+    result = services.collect_job_names(mock_session, [1, 2], "test", "token")
     assert result == expected_result
 
 
@@ -209,7 +176,9 @@ def test_collect_job_names_not_all_db():
     ), patch(
         "annotation.jobs.services.update_jobs_names"
     ) as mock_update_jobs_names:
-        result = collect_job_names(mock_session, [1, 2], "test", "token")
+        result = services.collect_job_names(
+            mock_session, [1, 2], "test", "token"
+        )
         mock_update_jobs_names.assert_called_once()
         assert result == expected_result
 
@@ -221,13 +190,15 @@ def test_get_jobs_by_name():
         (2, "test2"),
     )
     expected_result = {1: "test1", 2: "test2"}
-    result = get_jobs_by_name(mock_session, ("test1", "test2"), "test")
+    result = services.get_jobs_by_name(
+        mock_session, ("test1", "test2"), "test"
+    )
     assert result == expected_result
 
 
 def test_update_jobs_names():
     mock_session = MagicMock()
-    update_jobs_names(mock_session, {1: "test1", 2: "test2"})
+    services.update_jobs_names(mock_session, {1: "test1", 2: "test2"})
     expected_calls = [
         call({Job.name: "test1"}),
         call({Job.name: "test2"}),
@@ -241,8 +212,8 @@ def test_update_jobs_names():
 def test_update_jobs_categories_no_job(categories: Category):
     mock_session = MagicMock()
     mock_session.query().filter().with_for_update().first.return_value = None
-    with pytest.raises(JobNotFoundError):
-        update_jobs_categories(mock_session, "1", (categories,))
+    with pytest.raises(services.JobNotFoundError):
+        services.update_jobs_categories(mock_session, "1", (categories,))
 
 
 def test_update_jobs_categories(categories: Category):
@@ -253,12 +224,12 @@ def test_update_jobs_categories(categories: Category):
     mock_session.query().filter().with_for_update().first.return_value = (
         mock_job(1, mock_categories)
     )
-    update_jobs_categories(mock_session, "1", (categories,))
+    services.update_jobs_categories(mock_session, "1", (categories,))
     mock_categories.extend.assert_called_once()
     mock_session.commit.assert_called_once()
 
 
-def test_get_pages_in_work(tasks: Tuple[ManualAnnotationTask]):
+def test_get_pages_in_work(tasks: Tuple[ManualAnnotationTask, ...]):
     expected_result = {
         "validation": [
             {"file_id": tasks[0].file_id, "pages_number": tasks[0].pages}
@@ -267,13 +238,15 @@ def test_get_pages_in_work(tasks: Tuple[ManualAnnotationTask]):
             {"file_id": tasks[5].file_id, "pages_number": tasks[5].pages}
         ],
     }
-    result = get_pages_in_work({tasks[0], tasks[1], tasks[5]}, {tasks[1]})
+    result = services.get_pages_in_work(
+        {tasks[0], tasks[1], tasks[5]}, {tasks[1]}
+    )
     assert result == expected_result
 
 
-def test_get_tasks_to_delete(tasks: Tuple[ManualAnnotationTask]):
+def test_get_tasks_to_delete(tasks: Tuple[ManualAnnotationTask, ...]):
     expected_result = {tasks[4], tasks[3], tasks[2]}
-    result = get_tasks_to_delete(
+    result = services.get_tasks_to_delete(
         [tasks[0], tasks[1], tasks[2], tasks[3], tasks[4], tasks[5]]
     )
     assert result == expected_result
@@ -307,7 +280,9 @@ def test_get_jobs_by_files():
         "annotation.jobs.services.collect_job_names",
         return_value={1: "job_name_1", 2: "job_name_2"},
     ):
-        result = get_jobs_by_files(mock_session, {1, 2, 3}, "test", "test")
+        result = services.get_jobs_by_files(
+            mock_session, {1, 2, 3}, "test", "test"
+        )
         assert result == expected_result
 
 
@@ -320,7 +295,9 @@ def test_get_job_attributes_for_post_attribute_job(
         jobs_to_test_progress[0]
     )
     expected_result = jobs_to_test_progress[0]
-    result = get_job_attributes_for_post(mock_session, job_id, "test", (Job,))
+    result = services.get_job_attributes_for_post(
+        mock_session, job_id, "test", (Job,)
+    )
     assert result == expected_result
 
 
@@ -332,7 +309,7 @@ def test_get_job_attributes_for_post_attribute_columns():
         JobStatusEnumSchema.finished,
     )
     expected_result = ("job_name_2", JobStatusEnumSchema.finished)
-    result = get_job_attributes_for_post(
+    result = services.get_job_attributes_for_post(
         mock_session, job_id, "test", (Job.name, Job.status)
     )
     assert result == expected_result
@@ -343,4 +320,180 @@ def test_get_job_attributes_for_post_attribute_not_found():
     mock_session = MagicMock()
     mock_session.query().filter_by().first.return_value = None
     with pytest.raises(FieldConstraintError):
-        get_job_attributes_for_post(mock_session, job_id, "test", (Job.name,))
+        services.get_job_attributes_for_post(
+            mock_session, job_id, "test", (Job.name,)
+        )
+
+
+def test_recalculate_file_pages(files: Tuple[File]):
+    mock_session = MagicMock()
+    mock_result = MagicMock()
+    mock_session.query().filter.return_value = mock_result
+    expected_validating_pages = [5, 7]
+    expected_annotating_pages = [3, 5, 6, 7]
+
+    def filter_side_effect(*args):
+        if "NOT" in str(args[0]):
+            return MagicMock(
+                all=MagicMock(return_value=[([5, 6, 7],), ([3, 6],)])
+            )
+        else:
+            return MagicMock(all=MagicMock(return_value=[([5, 7],)]))
+
+    mock_result.filter.side_effect = filter_side_effect
+    services.recalculate_file_pages(mock_session, files[0])
+    assert files[0].distributed_validating_pages == expected_validating_pages
+    assert files[0].distributed_annotating_pages == expected_annotating_pages
+
+
+def test_read_user(users: Tuple[User]):
+    mock_session = MagicMock()
+    mock_session.query().get.return_value = users[0]
+    result = services.read_user(mock_session, users[0])
+    assert result == users[0]
+
+
+def test_create_user():
+    mock_session = MagicMock()
+    expected_result = User(user_id=1)
+    result = services.create_user(mock_session, 1)
+    assert result == expected_result
+
+
+@pytest.mark.skip(reason="can not create Job with JobSchema")
+def test_create_job():
+    mock_session = MagicMock()
+    expected_result = Job(
+        job_id=1,
+        name="test1",
+        callback_url="http://www.test.com",
+        annotators=[None],
+        files=[None],
+        is_auto_distribution=False,
+        categories=[None],
+        deadline=None,
+        job_type=JobTypeEnumSchema.ExtractionJob,
+        tenant="test",
+    )
+    result = services.create_job(
+        mock_session,
+        JobInfoSchema(
+            job_id=1,
+            name="test1",
+            callback_url="http://www.test.com",
+            annotators=set(),
+            files={1},
+            validators=set(),
+            owners={uuid.UUID("82533770-a99e-4873-8b23-6bbda86b59ae")},
+            previous_jobs=[],
+            datasets={1, 2},
+            is_auto_distribution=False,
+            categories=set(),
+            deadline=None,
+            job_type=JobTypeEnumSchema.ExtractionJob,
+            tenant="test",
+        ),
+    )
+    assert result == expected_result
+
+
+def test_add_users(users: Tuple[User, ...]):
+    mock_session = MagicMock()
+    cur_users = [users[1], users[2]]
+    new_user_ids = {UUID(int=3), UUID(int=4)}
+    expected_result = [
+        users[1],
+        users[2],
+        users[3],
+    ]
+    mock_session.query().filter().all.return_value = [users[3]]
+    result = services.add_users(mock_session, cur_users, new_user_ids)
+    assert result == expected_result
+
+
+def test_update_job_categories(categories: Category):
+    mock_session = MagicMock()
+    patch_data = {"categories": [1, 2, 3]}
+    expected_result = ["18d3d189e73a4680bfa77ba3fe6ebee5"]
+    tenat = "test"
+    with patch(
+        "annotation.jobs.services.fetch_bunch_categories_db",
+        return_value=[categories],
+    ):
+        services.update_job_categories(mock_session, patch_data, tenat)
+        assert [cat.id for cat in patch_data["categories"]] == expected_result
+
+
+@pytest.mark.parametrize(
+    "patch_data",
+    (
+        {
+            "annotators": [
+                User(user_id=UUID(int=1)),
+                User(user_id=UUID(int=2)),
+            ],
+        },
+        {"annotators": []},
+    ),
+)
+def test_validate_job_extensive_coverage_success(
+    patch_data: dict, users: Tuple[User, ...]
+):
+    job = Job(annotators=list(users[3:4]))
+    patch_data["extensive_coverage"] = 1
+    services.validate_job_extensive_coverage(patch_data, job)
+
+
+@pytest.mark.parametrize(
+    "patch_data",
+    (
+        {
+            "annotators": [
+                User(user_id=UUID(int=1)),
+                User(user_id=UUID(int=2)),
+            ],
+        },
+        {"annotators": []},
+    ),
+)
+def test_validate_job_extensive_coverage_error(
+    patch_data: dict, users: Tuple[User, ...]
+):
+    patch_data["extensive_coverage"] = 5
+    job = Job(annotators=list(users[3:4]))
+    with pytest.raises(FieldConstraintError):
+        services.validate_job_extensive_coverage(patch_data, job)
+
+
+def test_update_job_files(files: Tuple[File]):
+    mock_session = MagicMock()
+    patch_data = {"files": {1, 2}, "datasets": {"dataset1"}}
+    expected_calls = list(files[:2])
+    with patch(
+        "annotation.jobs.services.get_files_info",
+        return_value=[
+            {"file_id": 1, "pages_number": 100},
+            {"file_id": 2, "pages_number": 150},
+        ],
+    ):
+        services.update_job_files(
+            mock_session, patch_data, 1, "tenant", "token"
+        )
+        mock_session.add_all.assert_called_once_with(expected_calls)
+        mock_session.query().filter_by.assert_called_once_with(job_id=1)
+        mock_session.query().filter_by().delete.assert_called_once()
+
+
+def test_update_user_overall_load(tasks: Tuple[ManualAnnotationTask]):
+    mock_session = MagicMock()
+    mock_user = User(user_id=UUID(int=1))
+    mock_session.query().filter().all.return_value = (
+        tasks[0],
+        tasks[1],
+        tasks[2],
+        tasks[3],
+    )
+    mock_session.query().get.return_value = mock_user
+    services.update_user_overall_load(mock_session, UUID(int=1))
+    assert mock_user.overall_load == 3
+    mock_session.add.assert_called_once_with(mock_user)
