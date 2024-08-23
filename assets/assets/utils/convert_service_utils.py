b6ba25fc64d3d7cc190ed0b64f90d3c960f11aa0
Ruslan Khyurri <ruslan.khyurri@gmail.com>
2024-08-23 13:39:30 +0100

    feat: remove convert microservice

--- assets/assets/utils/convert_service_utils.py
+++ assets/assets/utils/convert_service_utils.py
@@ -1,7 +1,9 @@
 import requests
+from badgerdoc_storage import storage as bd_storage
 
 from assets import logger
 from assets.config import settings
+from assets.utils.convert.pdf import PDFToBadgerdocConverter
 
 LOGGER = logger.get_logger(__name__)
 
@@ -36,23 +38,16 @@ def post_txt_to_convert(
     LOGGER.info(f"File {input_text} successfully converted")
 
 
-def post_pdf_to_convert(bucket: str, input_pdf, output_tokens) -> None:
+def post_pdf_to_convert(tenant: str, input_pdf, output_tokens) -> None:
     """
+    TODO: rename this function
     Puts pdf from html file into convert service
     """
-    try:
-        response = requests.post(
-            url=settings.service_convert_pdf,
-            json={
-                "input_pdf": {"bucket": bucket, "path": input_pdf},
-                "output_tokens": {"bucket": bucket, "path": output_tokens},
-            },
-            headers={"x-current-tenant": bucket},
-        )
-        if response.status_code != 201:
-            raise UploadError(
-                "File %s failed to convert: %s", input_pdf, response.text
-            )
-    except requests.exceptions.ConnectionError as e:
-        LOGGER.error("Connection error - detail: %s", e)
-    LOGGER.info("File %s successfully converted", input_pdf)
+    LOGGER.debug(
+        "Converting file: %s, into output_tokens: %s", input_pdf, output_tokens
+    )
+    pdf_converter = PDFToBadgerdocConverter(bd_storage.get_storage(tenant))
+    pdf_converter.execute(
+        s3_input_pdf=input_pdf,
+        s3_output_tokens=output_tokens,
+    )
