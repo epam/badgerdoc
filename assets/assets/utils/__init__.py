4f1a6953d2149027529e541281f5dbaeeedbedac
AliBugday <ali_bugday@epam.com>
2025-04-28 17:31:04 +0300

    edit unused imports

--- assets/assets/utils/__init__.py
+++ assets/assets/utils/__init__.py
@@ -1,2 +1,6 @@
-from assets.utils import s3_utils  # noqa; noqa
-from assets.utils import common_utils, convert_service_utils, minio_utils
+from assets.utils import (  # noqa
+    common_utils,
+    convert_service_utils,
+    minio_utils,
+    s3_utils,
+)
