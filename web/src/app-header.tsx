2b46dc04e1e4e9a2dba92cca64df508946cae75a
Svetlana Sat <svetlana.sat88@gmail.com>
2025-07-31 19:11:47 +0400

    fix: Fix MainMenu item collapsing and overflow behavior

--- web/src/app-header.tsx
+++ web/src/app-header.tsx
@@ -1,6 +1,6 @@
 // temporary_disabled_rules
 /* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-redeclare, react-hooks/exhaustive-deps, no-restricted-globals */
-import React, { useContext, useMemo, useCallback } from 'react';
+import React, { useContext, useMemo, useCallback, ReactNode } from 'react';
 import { cloneDeep, isEmpty } from 'lodash';
 import {
     Dropdown,
@@ -15,7 +15,8 @@ import {
     MainMenuCustomElement,
     MainMenuDropdown,
     Text,
-    FlexSpacer
+    FlexSpacer,
+    Burger
 } from '@epam/loveship';
 import { useHistory } from 'react-router-dom';
 import { CurrentUser } from 'shared/contexts/current-user';
@@ -23,6 +24,7 @@ import { AppMenuItem } from 'api/typings';
 import { clearAuthDetails } from 'shared/helpers/auth-tools';
 import { useIsInIframe } from 'api/hooks/useIsInIframe';
 import { DASHBOARD_PAGE, DOCUMENTS_PAGE, MY_DOCUMENTS_PAGE } from 'shared';
+import { AdaptiveItemProps, MainMenuLogo } from '@epam/uui-components';
 
 export const AppHeader = () => {
     const history = useHistory();
@@ -79,6 +81,40 @@ export const AppHeader = () => {
             return { pathname: item.badgerdoc_path };
         }
     };
+
+    const renderAvatar = () =>
+        currentUser && (
+            <>
+                <FlexRow spacing="18">
+                    <Text color="white">{currentUser.current_tenant}</Text>|
+                    <Text color="white">{currentUser.username}</Text>
+                    <MainMenuCustomElement>
+                        <Dropdown
+                            renderTarget={(props) => (
+                                <MainMenuAvatar {...props} avatarUrl={avatarUrl} isDropdown />
+                            )}
+                            renderBody={() => (
+                                <DropdownMenuBody>
+                                    {currentUser.tenants.map((tenant) => (
+                                        <DropdownMenuButton
+                                            rawProps={{ 'data-tenant': tenant }}
+                                            caption={tenant}
+                                            key={tenant}
+                                            color="night"
+                                            onClick={setNewTenant}
+                                        />
+                                    ))}
+                                    <DropdownMenuSplitter />
+                                    <DropdownMenuButton caption="Log out" onClick={onLogOut} />
+                                </DropdownMenuBody>
+                            )}
+                            placement="bottom-end"
+                        />
+                    </MainMenuCustomElement>
+                </FlexRow>
+            </>
+        );
+
     const renderMenuButton = (item: AppMenuItem, isDropdown?: boolean) => (
         <MainMenuButton
             key={item.name}
@@ -86,73 +122,85 @@ export const AppHeader = () => {
             isLinkActive={pathMatches(item.badgerdoc_path)}
             link={getLinkTarget(item)}
             rawProps={{ 'data-page': item.badgerdoc_path }}
-            collapseToMore
-            priority={0}
             estimatedWidth={145}
             onClick={() => isDropdown && close()}
             {...(item.is_external ? { target: '_blank', rel: 'noopener noreferrer' } : {})}
         />
     );
 
+    const renderMenuItem = (item: AppMenuItem) => {
+        // Check if there are children (nesting level 2 maximum)
+        if (item.children && !isEmpty(item.children)) {
+            // Rendering a Dropdown with Child Items
+            const hasActiveChild = item.children.some((child: AppMenuItem) =>
+                pathMatches(child.badgerdoc_path)
+            );
+            return (
+                <MainMenuDropdown
+                    key={item.name}
+                    caption={item.name}
+                    isLinkActive={hasActiveChild}
+                    priority={menu.indexOf(item) + 1}
+                    estimatedWidth={128}
+                >
+                    {item.children.map((child: AppMenuItem) => renderMenuButton(child, true))}
+                </MainMenuDropdown>
+            );
+        }
+        // Rendering a simple MainMenuButton
+        return renderMenuButton(item);
+    };
+    const getMenuItems = (): AdaptiveItemProps<{ caption?: string; onClose?: () => void }>[] => {
+        return [
+            {
+                id: 'burger',
+                priority: 100,
+                collapsedContainer: true,
+                render: (item, hiddenItems) => (
+                    <Burger
+                        key={item.id}
+                        renderBurgerContent={(item) => {
+                            return (hiddenItems ?? [])
+                                .filter((i) => i.id !== 'burger' && i.id !== 'logo')
+                                .map((i) => i.render({ ...i, onClose: item.onClose }));
+                        }}
+                    />
+                )
+            },
+            {
+                id: 'logo',
+                priority: 99,
+                render: (param) => (
+                    <MainMenuLogo key={param.id} href={getLogoLink()} logoUrl="/svg/logo.svg" />
+                )
+            },
+            ...menu.map((item) => ({
+                id: item.name,
+                priority: menu.indexOf(item) + 1,
+                render: () => renderMenuItem(item)
+            })),
+            {
+                id: 'moreContainer',
+                priority: 8,
+                collapsedContainer: true,
+                render: (item, hiddenItems) => (
+                    <MainMenuDropdown caption="More" key={item.id}>
+                        {hiddenItems?.map((i) => i.render({ ...i, onClose: item.onClose }))}
+                    </MainMenuDropdown>
+                )
+            },
+            { id: 'flexSpacer', priority: 100, render: (p) => <FlexSpacer key={p.id} /> },
+            {
+                id: 'account',
+                priority: 100,
+                render: () => renderAvatar()
+            }
+        ];
+    };
     if (isInIframe) return null;
     return (
         <FlexCell>
-            <MainMenu appLogoUrl="/svg/logo.svg" logoHref={getLogoLink()}>
-                {menu.map((item) => {
-                    // Check if there are children (nesting level 2 maximum)
-                    if (item.children && !isEmpty(item.children)) {
-                        // Rendering a Dropdown with Child Items
-                        const hasActiveChild = item.children.some((child: AppMenuItem) =>
-                            pathMatches(child.badgerdoc_path)
-                        );
-                        return (
-                            <MainMenuDropdown
-                                key={item.name}
-                                caption={item.name}
-                                isLinkActive={hasActiveChild}
-                                priority={2}
-                                estimatedWidth={128}
-                            >
-                                {item.children.map((child: AppMenuItem) =>
-                                    renderMenuButton(child, true)
-                                )}
-                            </MainMenuDropdown>
-                        );
-                    }
-                    // Rendering a simple MainMenuButton
-                    return renderMenuButton(item);
-                })}
-                <FlexSpacer />
-                {currentUser && (
-                    <FlexRow spacing="18">
-                        <Text color="white">{currentUser.current_tenant}</Text>|
-                        <Text color="white">{currentUser.username}</Text>
-                        <MainMenuCustomElement>
-                            <Dropdown
-                                renderTarget={(props) => (
-                                    <MainMenuAvatar {...props} avatarUrl={avatarUrl} isDropdown />
-                                )}
-                                renderBody={() => (
-                                    <DropdownMenuBody>
-                                        {currentUser.tenants.map((tenant) => (
-                                            <DropdownMenuButton
-                                                rawProps={{ 'data-tenant': tenant }}
-                                                caption={tenant}
-                                                key={tenant}
-                                                color="night"
-                                                onClick={setNewTenant}
-                                            />
-                                        ))}
-                                        <DropdownMenuSplitter />
-                                        <DropdownMenuButton caption="Log out" onClick={onLogOut} />
-                                    </DropdownMenuBody>
-                                )}
-                                placement="bottom-end"
-                            />
-                        </MainMenuCustomElement>
-                    </FlexRow>
-                )}
-            </MainMenu>
+            <MainMenu items={getMenuItems()} />
         </FlexCell>
     );
 };
