dd69db0bf1cd6896e9265078e5750b738dfaf7b9
Andrei Sakirkin <andrei_sakirkin@epam.com>
2026-05-15 09:15:12 +0300

    addressed a review comment

--- web/frontend/src/shared/api/adapters/real/users.ts
+++ web/frontend/src/shared/api/adapters/real/users.ts
@@ -7,7 +7,7 @@ interface BadgerDocUserResponse {
   username: string
   first_name: string
   last_name: string
-  is_admin: true
+  is_admin: boolean
 }
 
 export const realUsersAdapter: UsersAdapter = {
